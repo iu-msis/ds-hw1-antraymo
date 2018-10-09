5dfe526a0e3709bfd378a142e2b3b256fd15e429
AJ Raymond <antraymo@iu.edu>
2018-10-09 15:50:12 -0400

    update table

--- app/data/hw1.sql
+++ app/data/hw1.sql
@@ -5,9 +5,9 @@ CREATE TABLE hw1 (
   comment VARCHAR (200) NOT NULL
 );
 
-INSERT INTO Feedback (id, comment)
+INSERT INTO hw1 (id, comment)
 VALUES (1, "This website is trash!");
-INSERT INTO Feedback (id, comment)
+INSERT INTO hw1 (id, comment)
 VALUES (2, "I am neutral to this website");
-INSERT INTO Feedback (id, comment)
+INSERT INTO hw1 (id, comment)
 VALUES (3, "I really like this website!");
